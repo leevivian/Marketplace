af0fd8e7bbc82dc79a71d4030ddb7bf2ffbe1908
Steven-Soult-Jr <ssoult@mail.sfsu.edu>
2017-02-11 01:56:01 -0800

    About page v1 Set up a default about page, with a basic example for myself.

---
+++ application/views/pages/about.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>About Page</title>
+    </head>
+    <body>
+        <h1 style="text-align:center;">SFSU Software Engineering Class</h1>
+        <h1 style="text-align:center;">Spring 2017</h1>
+        <h1 style="text-align:center;">Section 2</h1>
+        <h1 style="text-align:center;">Team 12</h1>
+        <p><a href=<?php echo site_url($uri = "about/members/aculanay") ?>>AJ Culanay</a></p>
+        <p><a href=<?php echo site_url($uri = "about/members/nhoffman") ?>>Nick Hoffman</a></p>
+        <p><a href=<?php echo site_url($uri = "about/members/vlee") ?>>Vivian Lee</a></p>
+        <p><a href=<?php echo site_url($uri = "about/members/mserna") ?>>Matthew Serna</a></p>
+        <p><a href=<?php echo site_url($uri = "about/members/ssoult") ?>>Steven Soult</a></p>
+        <p><a href=<?php echo site_url($uri = "about/members/jvinter-jensen") ?>>Jonas Vinter-Jensen</a></p>
+    </body>
+</html>
---
+++ application/views/pages/about_soult.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>About Steven Soult</title>
+    </head>
+    <body>
+        <h1 style="text-align: center;">Steven Soult</h1>
+        <p>TODO - Insert Picture</p>
+    </body>
+</html>
