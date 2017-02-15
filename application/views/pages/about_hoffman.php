c83b39924c002818936cc0af371c238479f28306
nchoffman <f6t3a92>
2017-02-14 18:13:13 -0800

    adding about file for 'Nick Hoffman'

---
+++ application/views/pages/about_hoffman.php
@@ -0,0 +1,17 @@
+
+/**
+ * Created by PhpStorm.
+ * User: Nick_2
+ * Date: 2/14/2017
+ * Time: 6:08 PM
+ */
+
+<html>
+    <head>
+        <title>About Nicholas Hoffman</title>
+    </head>
+    <body>
+        <h1 style="text-align: center;">Nicholas Hoffman</h1>
+        <p>TODO - Insert Picture</p>
+    </body>
+</html>
