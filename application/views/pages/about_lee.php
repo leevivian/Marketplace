8969d9972c2b1fab5df3273b2519bdbeb819bd95
Vivian Lee <vivianlee8295@gmail.com>
2017-02-14 20:56:23 -0800

    Update Vivian's about me page

--- application/views/pages/about_lee.php
+++ application/views/pages/about_lee.php
@@ -1,14 +1,8 @@
 <html>
-    <head>
-        <title>About Vivian Lee</title>
-    </head>
-    <body>
-    <h1 style="text-align: center;">Vivian Lee</h1>
-
-    <div align="center">
-        <p>
-            <img src="/images/vlee.jpg" alt="Vivian" width="200" height="200">
-        </p>
-    </div>
-    </body>
+<div style="clear: both; text-align: center;">
+    <span style="font-family: &quot;courier new&quot; , &quot;courier&quot; , monospace; font-size: large;">About Vivian Lee</span></div>
+<br />
+<div style="text-align: center;">
+    <a href="https://4.bp.blogspot.com/-h5AqwsggBAw/WKPect1HxsI/AAAAAAAAAHM/RwKN5Yg9eQ0hm0W6DcZQwNBypHCUo8a6gCLcB/s1600/vlee.jpg" imageanchor="1" style="margin-left: 1em; margin-right: 1em;"><img border="0" src="https://4.bp.blogspot.com/-h5AqwsggBAw/WKPect1HxsI/AAAAAAAAAHM/RwKN5Yg9eQ0hm0W6DcZQwNBypHCUo8a6gCLcB/s1600/vlee.jpg" /></a>
+</div>
 </html>
