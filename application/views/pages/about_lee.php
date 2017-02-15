13d248402925129c86358346ac75f89477f173e3
Vivian Lee <vivianlee8295@gmail.com>
2017-02-14 20:46:53 -0800

    Add about_lee & images folder

---
+++ application/views/pages/about_lee.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>About Vivian Lee</title>
+    </head>
+    <body>
+    <h1 style="text-align: center;">Vivian Lee</h1>
+
+    <div align="center">
+        <p>
+            <img src="/images/vlee.jpg" alt="Vivian" width="200" height="200">
+        </p>
+    </div>
+    </body>
+</html>
