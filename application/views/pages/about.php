6bd0688e67df1152305433426e7737909b5b0875
Steven-Soult-Jr <ssoult@mail.sfsu.edu>
2017-02-14 17:43:25 -0800

    About page v2 Working on my local server.

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -7,11 +7,11 @@
         <h1 style="text-align:center;">Spring 2017</h1>
         <h1 style="text-align:center;">Section 2</h1>
         <h1 style="text-align:center;">Team 12</h1>
-        <p><a href=<?php echo site_url($uri = "about/members/aculanay") ?>>AJ Culanay</a></p>
-        <p><a href=<?php echo site_url($uri = "about/members/nhoffman") ?>>Nick Hoffman</a></p>
-        <p><a href=<?php echo site_url($uri = "about/members/vlee") ?>>Vivian Lee</a></p>
-        <p><a href=<?php echo site_url($uri = "about/members/mserna") ?>>Matthew Serna</a></p>
-        <p><a href=<?php echo site_url($uri = "about/members/ssoult") ?>>Steven Soult</a></p>
-        <p><a href=<?php echo site_url($uri = "about/members/jvinter-jensen") ?>>Jonas Vinter-Jensen</a></p>
+        <p><a href="index.php/about/members/aculanay">AJ Culanay</a></p>
+        <p><a href="index.php/about/members/nhoffman">Nick Hoffman</a></p>
+        <p><a href="index.phpabout/members/vlee">Vivian Lee</a></p>
+        <p><a href="index.php/about/members/mserna">Matthew Serna</a></p>
+        <p><a href="index.php/about/members/ssoult">Steven Soult</a></p>
+        <p><a href="index.php/about/members/jvinter-jensen">Jonas Vinter-Jensen</a></p>
     </body>
 </html>
