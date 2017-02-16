c708d9c2015beb29a7e3772dc2eb22db1f4f3f38
Vivian Lee <vivianlee8295@gmail.com>
2017-02-16 12:29:32 -0800

    Merge branch 'ssoult' of https://github.com/aculanay/csc648team12 into Vivian

--- application/views/pages/about_lee.php
+++ application/views/pages/about_lee.php
@@ -1,8 +1,60 @@
-<html>
-<div style="clear: both; text-align: center;">
-    <span style="font-family: &quot;courier new&quot; , &quot;courier&quot; , monospace; font-size: large;">About Vivian Lee</span></div>
-<br />
-<div style="text-align: center;">
-    <a href="https://4.bp.blogspot.com/-h5AqwsggBAw/WKPect1HxsI/AAAAAAAAAHM/RwKN5Yg9eQ0hm0W6DcZQwNBypHCUo8a6gCLcB/s1600/vlee.jpg" imageanchor="1" style="margin-left: 1em; margin-right: 1em;"><img border="0" src="https://4.bp.blogspot.com/-h5AqwsggBAw/WKPect1HxsI/AAAAAAAAAHM/RwKN5Yg9eQ0hm0W6DcZQwNBypHCUo8a6gCLcB/s1600/vlee.jpg" /></a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Base theme taken from www.w3schools.com - No Copyright -->
+    <title>About Vivian Lee</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css');?>">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="<?php echo base_url('assets/js/jquery-3.1.1.min.js');?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
+    <style>
+        body {
+            font: 20px Montserrat, sans-serif;
+            line-height: 1.8;
+            color: #f5f6f7;
+        }
+        p {font-size: 16px;}
+        .margin {margin-bottom: 45px;}
+        .bg-1 {
+            background-color: #1abc9c; /* Green */
+            color: #ffffff;
+        }
+        .bg-2 {
+            background-color: #474e5d; /* Dark Blue */
+            color: #ffffff;
+        }
+        .bg-3 {
+            background-color: #ffffff; /* White */
+            color: #555555;
+        }
+        .bg-4 {
+            background-color: #2f2f2f; /* Black Gray */
+            color: #fff;
+        }
+        .container-fluid {
+            padding-top: 70px;
+            padding-bottom: 70px;
+        }
+        .navbar {
+            padding-top: 15px;
+            padding-bottom: 15px;
+            border: 0;
+            border-radius: 0;
+            margin-bottom: 0;
+            font-size: 12px;
+            letter-spacing: 5px;
+        }
+        .navbar-nav  li a:hover {
+            color: #1abc9c !important;
+        }
+    </style>
+</head>
+<body>
+
+<!-- First Container -->
+<div class="container-fluid bg-1 text-center">
+    <h3 class="margin">Vivian Lee</h3>
+    <img src="<?php echo base_url('images/lee.JPG'); ?>" class="img-responsive img-circle margin" style="display:inline" alt="Picture" width="350" height="350">
 </div>
-</html>
