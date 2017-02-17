46318b8e1718eb5d1d63b4ef94f0c3c673a3a067
jonasvinter-jensen <jonasvinter-jensen@jonass-mac-mini>
2017-02-17 00:07:58 -0800

    My about page.

---
+++ application/views/pages/about_vinter-jensen.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>About</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  
+<div class="container-fluid">    
+<br>
+  <p>My name is Jonas Vinter-Jensen and I am taking Software Engineering at San Francisco State University, Spring 2017. 
+  <br>The goal was to make a web application which involves buying/selling products between students at SFSU. </p>
+  <img src="<?php echo base_url('images/public_domain_photo.jpg'); ?>" alt="Snowy Mountain" style="width:400px;height:300px;">
+</div>
+
+</body>
+</html>
