5bf1082a12899754cafc5fb687d8156b614d750c
mserna <mserna@mail.sfsu.edu>
2017-02-16 19:09:40 -0800

    Merge branch 'master' of https://github.com/aculanay/csc648team12 into mserna
    # Conflicts:
    #	application/controllers/About.php

--- application/views/pages/about_mserna.php
+++ application/views/pages/about_mserna.php
@@ -56,5 +56,5 @@
 <!-- First Container -->
 <div class="container-fluid bg-1 text-center">
     <h3 class="margin">Matthew Serna</h3>
-    <img src="<?php echo base_url(); ?>" class="img-responsive img-circle margin" style="display:inline" alt="Picture" width="350" height="350">
+    <img src="<?php echo base_url('images/serna.jpg'); ?>" class="img-responsive img-circle margin" style="display:inline" alt="Picture" width="350" height="350">
 </div>
