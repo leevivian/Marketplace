a53711d0faa87fe7e5a506dfaf33d3c72099627c
Steven-Soult-Jr <ssoult@mail.sfsu.edu>
2017-02-17 15:47:14 -0800

    Fixing issues from the merge Everything is working and uploaded to the server. It looks good.

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
-$config['base_url'] = 'sfsuse.com/~nchoffman/';
+$config['base_url'] = 'http://sfsuse.com/~sp17g12';
 
 /*
 |--------------------------------------------------------------------------
