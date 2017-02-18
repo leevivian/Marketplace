93ab52e1e824c72592f91b497f395681f6a7bf61
jonasvinter-jensen <jonasvinter-jensen@jonass-mac-mini>
2017-02-17 16:51:09 -0800

    Updated about page to use the bootstrap template. V2.

--- application/views/pages/about_vinter-jensen.php
+++ application/views/pages/about_vinter-jensen.php
@@ -56,7 +56,5 @@
 <!-- First Container -->
 <div class="container-fluid bg-1 text-center">
     <h3 class="margin">Jonas Vinter-Jensen</h3>
-    <p>My name is Jonas Vinter-Jensen and I am taking Software Engineering at San Francisco State University, Spring 2017. 
-    <br>The goal was to make a web application which involves buying/selling products between students at SFSU. </p>
-    <img src="<?php echo base_url('images/public_domain_photo.jpg'); ?>" class="img-responsive img-circle margin" style="display:inline" alt="Picture" width="350" height="350">
+    <img src="<?php echo base_url('images/public_domain_photo.jpg'); ?>" class="img-responsive img-circle margin" style="display:inline" alt="Snowy Mountain" width="350" height="350">
 </div>
