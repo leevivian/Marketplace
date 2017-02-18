c8301417c358d438956d506e35837830e981bb92
jonasvinter-jensen <jonasvinter-jensen@jonass-mac-mini>
2017-02-17 17:05:16 -0800

    Updated about page to use the bootstrap template. V2.

--- application/views/pages/about_vinter-jensen.php
+++ application/views/pages/about_vinter-jensen.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <!-- Base theme taken from www.w3schools.com - No Copyright -->
-    <title>About AJ Culanay</title>
+    <title>About Jonas Vinter-Jensen</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css');?>">
