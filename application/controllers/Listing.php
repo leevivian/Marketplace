3e529568c5c56d6dfad0e407fde1112da1d26673
mserna <mserna@mail.sfsu.edu>
2017-04-07 16:28:05 -0700

    Merge branches 'master' and 'mserna' of https://github.com/aculanay/csc648team12 into mserna
    # Conflicts:
    #	application/views/pages/about_mserna.php

--- application/controllers/Listing.php
+++ application/controllers/Listing.php
@@ -8,10 +8,20 @@
  */
 class Listing extends CI_Controller
 {
-    public function index(){
+    public function index()
+    {
         $title = array('title' => 'Search Listing');
         $this->load->view('header', $title);
         //Specific page
         $this->load->view('footer');
     }
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Listings_model');
+        $this->load->helper('form');
+
+    }
+
 }
