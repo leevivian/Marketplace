8be252f15998a7d79e6241e69b85d86a7ab24cdd
mserna <mserna@mail.sfsu.edu>
2017-04-10 09:51:49 -0700

    Updated the Config.php file to point to my personal server. Re-ordered the functions in Listing.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://sfsuse.com/~sp17g12';
+$config['base_url'] = 'http://sfsuse.com/~mserna';
 
 /*
 |--------------------------------------------------------------------------
