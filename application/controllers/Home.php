62ce420c9c450b60efdce32cde1f3d9e37da3aeb
Vivian Lee <vivianlee8295@gmail.com>
2017-04-10 19:20:31 -0700

    Add constructor for Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,6 +8,11 @@
  */
 class Home extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     public function index()
     {
         $title = array('title' => 'Home Page');
