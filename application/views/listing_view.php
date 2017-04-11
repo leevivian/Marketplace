7d1a3e26236bbb926a9a03504b707ea52dc6b19c
mserna <mserna@mail.sfsu.edu>
2017-04-10 20:28:57 -0700

    Text color change to black

--- application/views/listing_view.php
+++ application/views/listing_view.php
@@ -6,6 +6,13 @@
  */
 
 <html>
+
+<style>
+    p{
+        color: black;  
+    }
+</style>
+
 <div class="container-fluid text-center">
     <div class="row content">
         <div class="col-sm-2 sidenav">
