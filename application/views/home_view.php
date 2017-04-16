8b5ff44adf35105c1e48a8e4df659194222f5333
Vivian Lee <vivianlee8295@gmail.com>
2017-04-16 00:53:58 -0700

    Update carousel's images

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -35,15 +35,15 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-                <img src="<?php echo base_url()?>images/c2.jpg" alt="Chania" width="460" height="345">
+                <img src="<?php echo base_url()?>images/car2.jpg" alt="Chania" width="460" height="345">
             </div>
 
             <div class="item">
-                <img src="<?php echo base_url()?>images/c1.jpg" alt="Chania" width="460" height="345">
+                <img src="<?php echo base_url()?>images/car1.jpg" alt="Chania" width="460" height="345">
             </div>
 
             <div class="item">
-                <img src="<?php echo base_url()?>images/c3.jpg" alt="Flower" width="460" height="345">
+                <img src="<?php echo base_url()?>images/car3.jpg" alt="Flower" width="460" height="345">
             </div>
 
         </div>
