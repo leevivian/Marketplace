8752e74f08ca148cec1c3acc84cead13b6163123
Steven-Soult-Jr <ssoult@mail.sfsu.edu>
2017-04-17 16:51:56 -0700

    Updated Messaging page Added the ability to click on a message and display it's contents. It is ugly and dumb, but working.

--- application/views/messaging/messaging_view.php
+++ application/views/messaging/messaging_view.php
@@ -1,39 +1,15 @@
-<?php session_start();
-    if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true) : ?>
-<div class="container-fluid bg-2 text-left">
-    <h2 style="text-decoration: underline">Inbox</h2>
-    <h2 style="text-decoration: underline">Sent By: Subject: Time: </h2>
-    <?php
-    if(isset($messages)) {
-        foreach($messages as $row) {
-            if($row['read'] == false) {
-                echo "<b>{$row['sender']} - {$row['title']} - {$row['timestamp']}</b>";
-            } else {
-                echo "<p>{$row['sender']} - {$row['title']} - {$row['timestamp']}</p>";
-            }
-        }
-    } else {
-        echo "Error loading data.";
-    }
-    ?>
-</div>
-<?php else : ?>
-<!--
-<div class="container-fluid bg-2 text-center">
-    <p style="text-align:center;">You must be logged in to see this page.</p>
-</div> 
--->
 <div class="container-fluid bg-2 text-left">
     <h2 style="text-decoration: underline">Inbox</h2>
     <?php
-    if(isset($messages)) {
-        if(isset($expand)) {
-            echo "<table style=\"width:100%\"";
+    if(isset($message)) {
+        echo "<table style=\"width:100%\"";
             echo "<tr><th>Author</th><th>Subject</th><th>Contents</th></tr>";
-            echo "<tr><th>{$expand['sender']}</th><th>{$expand['subject']}</th><th>{$expand['contents']}</th></tr>";
+            foreach($message as $row) {
+            echo "<tr><th>{$row['sender']}</th><th>{$row['subject']}</th><th>{$row['contents']}</th></tr>";
+            }
             echo "</table>";
             echo "<a href=\"".base_url()."index.php/messaging\" class=\"btn btn-primary\">Back</a>";
-        } else {
+    } else if(isset($messages)) {
             echo "<table style=\"width:100%\">";
             echo "<tr><th>Read</th><th>Author</th><th>subject</th><th>time sent</th></tr>";
             foreach($messages as $row) {
@@ -42,17 +18,17 @@
                 $dt->setTimezone(new DateTimeZone('PDT'));
                 $time = $dt->format('h:ia M j');
                 if($row['read'] == false) {
-                    echo "<tr><th>O</th>";
+                    echo "<tr><th><a href='messaging/open/{$row['messageid']}'>O</a></th>";
                 } else {
-                    echo "<tr><th>X</th>";
+                    echo "<tr><th><a href='messaging/open/{$row['messageid']}'>X</a></th>";
                 }
-            echo "<th>{$row['sender']}</th><th>{$row['subject']}</th><th>$time</th></tr>";
+            echo "<th><a href='messaging/open/{$row['messageid']}'>{$row['sender']}</a></th>"
+            . "<th><a href='messaging/open/{$row['messageid']}'>{$row['subject']}</a></th>"
+            . "<th><a href='messaging/open/{$row['messageid']}'>$time</a></th></tr>";
         }
         echo "</table>";
-        }
     } else {
         echo "Error loading data.";
     }
     ?>
 </div>
-<?php endif; ?>
