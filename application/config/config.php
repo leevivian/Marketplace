161d52c4baff8b937362fda0e234eee5c1151bcc
nchoffman <f6t3a92>
2017-04-18 11:30:15 -0700

    Merge branches 'development' and 'nick' of https://github.com/aculanay/csc648team12 into nickH
    Added contact page and contact submission page/view.
    
    Conflicts:
    	application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://sfsuse.com/~sp17g12';
+$config['base_url'] = 'http://sfsuse.com/~nchoffma';
 
 /*
 |--------------------------------------------------------------------------
