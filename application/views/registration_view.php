42c5b3bcef33979b77105739c67c95a787465be6
Vivian Lee <vivianlee8295@gmail.com>
2017-05-02 20:41:31 -0700

    Format error messages on Registration page

--- application/views/registration_view.php
+++ application/views/registration_view.php
@@ -57,7 +57,8 @@
                 <div class="form-group row">
                     <label class="control-label col-md-4" for="username">First Name: </label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" id="first-name" name="firstname" required="true" placeholder="First Name">
+                        <input type="text" class="form-control" id="firstname" name="firstname" required="true" placeholder="First Name"
+                               value="<?php echo set_value('firstname');?>">
                         <?php echo form_error('firstname', '<div class="error">', '</div>'); ?>
                     </div>
                 </div>
@@ -65,7 +66,8 @@
                 <div class="form-group row">
                     <label class="control-label col-md-4" for="username">Last Name: </label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" id="last-name" name="lastname" required="true" placeholder="Last Name">
+                        <input type="text" class="form-control" id="lastname" name="lastname" required="true" placeholder="Last Name"
+                               value="<?php echo set_value('lastname');?>">
                         <?php echo form_error('lastname', '<div class="error">', '</div>'); ?>
                     </div>
                 </div>
@@ -73,7 +75,8 @@
                 <div class="form-group row">
                     <label class="control-label col-md-4" for="username">Username: </label>
                     <div class="col-md-8">
-                        <input type="text" class="form-control" id="username" name="username" required="true" placeholder="Username">
+                        <input type="text" class="form-control" id="username" name="username" required="true" placeholder="Username"
+                               value="<?php echo set_value('username');?>">
                         <?php echo form_error('username', '<div class="error">', '</div>'); ?>
                     </div>
                 </div>
@@ -106,7 +109,7 @@
                 </div>
 
                 <div class="form-group row checkbox-inline" style="padding-left: 80px;">
-                    <label><input type="checkbox" style="height: 30px;" value="">I have read the terms & agreements.</label>
+                    <label><input type="checkbox" required="true" style="height: 30px;" value="">I have read the terms & agreements.</label>
                 </div>
 
                 <div class="form-group row">
