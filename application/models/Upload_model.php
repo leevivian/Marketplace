4c5f970170cb9da7bcfb4188085268dc37c7fcc0
AJ Culanay <aculanay@mail.sfsu.edu>
2017-05-03 22:46:14 -0700

    didn't add files from previous commit

--- application/models/Upload_model.php
+++ application/models/Upload_model.php
@@ -10,12 +10,18 @@ class Upload_model extends CI_Model{
     public function __construct() {
         parent::__construct();
         $this->load->database();
+        $this->load->model('Search_model');
     }
 
     public function insert_item($item){
 
-        return $this->db->insert('Items', $item);
+        $this->db->insert('Items', $item);
 
+        $itemid = $this->db->insert_id();
+        $data['results']= $this->search_model->get_details($itemid);
+        $this->load->view('header');
+        $this->load->view('listingPage_view', $data);
+        $this->load->view('footer');
     }
 
 }
