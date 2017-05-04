17da03433e9ca4807f47f53b9e414b8db485ea76
Vivian Lee <vivianlee8295@gmail.com>
2017-05-03 21:43:34 -0700

    Migrate sending messages page to sendMessage_view

--- application/views/messaging/sendMessage_view.php
+++ application/views/messaging/sendMessage_view.php
@@ -17,27 +17,56 @@
     <div class="row">
         <div class="col-md-12">
             <div class="well well-sm">
-                <form class="form-horizontal" method="post">
+
+                <?php
+                if(isset($sent)):
+                    switch ($sent) {
+                        case 1:
+                            echo "Message Sent.<br>";
+                            echo "<a href=\"" . base_url() . "index.php/messaging\" class=\"btn btn-primary\">Back to Inbox</a>";
+                            break;
+                        case -1:
+                            echo "User not found.<br>";
+                            echo "<a href=\"" . base_url() . "index.php/messaging\" class=\"btn btn-primary\">Back to Inbox</a>";
+                            break;
+                        case 0:
+                            echo "An unknown error occured. Please contact the administrator.";
+                            echo "<a href=\"" . base_url() . "index.php/messaging/send/Admin\" class=\"btn btn-primary\">Back to Inbox</a>";
+                            echo "<a href=\"" . base_url() . "index.php/messaging\" class=\"btn btn-primary\">Back to Inbox</a>";
+                            break;
+                    }
+                else:
+                if (!isset($recipient)) {
+                    $recipient = "";
+                }
+                if (!isset($subject)) {
+                    $subject = "";
+                }
+                echo form_open('messaging/send_message'); ?>
+
+                <form class="form-horizontal">
                     <fieldset>
                         <legend class="text-center header">Send Message</legend>
 
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-1 text-right">To:</span>
                             <div class="col-md-8">
-                                <input id="recipient-field" name="recipient" type="text" placeholder="Recipient" class="form-control">
+                                <input id="recipient-field" name="recipient" type="text" placeholder="Recipient" class="form-control"
+                                    value=<?php echo $recipient ?>>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-1 text-center">Subject:</span>
                             <div class="col-md-8">
-                                <input id="subject-field" name="subject" type="text" placeholder="Subject" class="form-control">
+                                <input id="subject-field" name="subject" type="text" placeholder="Subject" class="form-control"
+                                    value=<?php echo $subject ?>>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-2">
-                                <textarea class="form-control" id="message" name="message" placeholder="Enter your message here." rows="7"></textarea>
+                                <textarea class="form-control" id="contents" name="conents" placeholder="Enter your message here." rows="7"></textarea>
                             </div>
                         </div>
 
@@ -48,6 +77,8 @@
                         </div>
                     </fieldset>
                 </form>
+
+                <?php endif; ?>
             </div>
         </div>
     </div>
