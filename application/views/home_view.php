efe3103bcac797ccdb6527626173e4d5162274a5
Vivian Lee <vivianlee8295@gmail.com>
2017-05-03 22:46:13 -0700

    Add actual recent listings to home page, plus functional More Info button

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -34,83 +34,36 @@
         <!-- Page Features -->
         <div class="row text-center">
 
-            <div class="col-md-2 col-sm-6 hero-feature">
-                <div class="thumbnail" style="min-height: 386px; max-height: 386px;">
-                    <img src="<?php echo base_url()?>images/temp_images/b1.jpg" alt="" style="height: 200px">
-                    <div class="caption" style="176px;">
-                        <h4>CSC675 Intro to Database Systems</h4>
-                        <p>$20.00</p>
-                        <p>
-                            <a href="<?php echo base_url()?>index.php/individual_listing" class="btn btn-default">More Info</a>
-                        </p>
+            <?php
+            if(isset($recent_listings)) {
+                foreach($recent_listings as $row) {
+                    $id = $row['itemid'];
+                    ?>
+                    <div class='col-md-2 col-sm-6 hero-feature'>
+                        <div class='thumbnail' style='min-height: 386px; max-height: 386px;''>
+                        <img src="<?php echo base_url('images/item_images/' . 'thumbnail_' . $row['image']); ?>" alt="Image not found"
+                             style="height: 200px">
+                        <div class="caption" style="176px;">
+                            <h4><?php
+                                echo $row['name'];
+                                ?>
+                            </h4>
+                            <p>
+                                <?php
+                                echo "$" . $row['price'];
+                                ?>
+                            </p>
+                            <p>
+                                <a href="<?php echo base_url()?>index.php/search/load_details/<?php echo $id?>">
+                                    <button class="btn btn-primary" type="submit" >More Info</button></a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-md-2 col-sm-6 hero-feature">
-                <div class="thumbnail" style="min-height: 386px; max-height: 386px;">
-                    <img src="<?php echo base_url()?>images/temp_images/b2.jpg" alt="" style="height: 200px">
-                    <div class="caption">
-                        <h4>CSC415 Operating Systems</h4>
-                        <p>$35.00</p>
-                        <p>
-                            <a href="<?php echo base_url()?>index.php/individual_listing" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-2 col-sm-6 hero-feature">
-                <div class="thumbnail" style="min-height: 386px; max-height: 386px;">
-                    <img src="<?php echo base_url()?>images/temp_images/b3.jpg" alt="" style="height: 200px">
-                    <div class="caption">
-                        <h4>Harry Potter and the Half-Blood Prince</h4>
-                        <p>$15.00</p>
-                        <p>
-                            <a href="<?php echo base_url()?>index.php/individual_listing" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-2 col-sm-6 hero-feature">
-                <div class="thumbnail" style="min-height: 386px; max-height: 386px;">
-                    <img src="<?php echo base_url()?>images/temp_images/b4.jpg" alt="" style="height: 200px">
-                    <div class="caption" style="176px;">
-                        <h4>French for Dummies Audio Set</h4>
-                        <p>$20.00</p>
-                        <p>
-                            <a href="<?php echo base_url()?>index.php/individual_listing" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-2 col-sm-6 hero-feature">
-                <div class="thumbnail" style="min-height: 386px; max-height: 386px;">
-                    <img src="<?php echo base_url()?>images/temp_images/b5.jpg" alt="" style="height: 200px">
-                    <div class="caption" style="176px;">
-                        <h4>Book of Spells for Grade 5 and Above</h4>
-                        <p>$20.00</p>
-                        <p>
-                            <a href="<?php echo base_url()?>index.php/individual_listing" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-2 col-sm-6 hero-feature">
-                <div class="thumbnail" style="min-height: 386px; max-height: 386px;">
-                    <img src="<?php echo base_url()?>images/temp_images/b6.jpeg" alt="" style="height: 200px">
-                    <div class="caption" style="176px;">
-                        <h4>iPad Mini Refurbished 2017</h4>
-                        <p>$200.00</p>
-                        <p>
-                            <a href="<?php echo base_url()?>index.php/individual_listing" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
+                <?php
+                }
+            }
+            ?>
 
         </div>
     </div>
@@ -183,15 +136,6 @@
 
 </style>
 
-    <?php
-    /**
-     * Created by PhpStorm.
-     * User: Vivian
-     * Date: 4/10/17
-     * Time: 7:07 PM
-     */
-
-    ?>
 
 </html>
 
