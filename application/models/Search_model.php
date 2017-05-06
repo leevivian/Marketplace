1a9a24701f92613102dd65df90cbfa84ee52d92e
AJ Culanay <aculanay@mail.sfsu.edu>
2017-05-05 20:54:33 -0700

    Fixed query building

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -10,58 +10,35 @@ class Search_model extends CI_Model {
     public function get_items($search, $category, $condition){
         $search = trim($search);
 
-       /* //searches the database's 'items' table for similarity in 'name' and 'category' columns
-        if($category=="All")
-        {
-            $this->db->select('*');
-        }
-        else if($category=="Other")
-        {
-            $category=NULL; //If category was set to Other, choose any database item with no category 
-            $this->db->where('category', $category);
-        }
-        else 
-        {
-            $this->db->where('category', $category);
-        }
+        // SELECT * FROM Items
+
+        $this->db->select('*');
         $this->db->from('Items');
-        if(isset($search) && $search != "")
-        {
+
+        if($category === "All" && $condition === "Any"){
+            // WHERE name LIKE $search
+
             $this->db->like('name', $search);
-            $this->db->or_like('description', $search);
+        }else if($category !== "All" && $condition === "Any"){
+
+            // WHERE category = $category AND WHERE name LIKE $search
+            $this->db->where('category', $category);
+        }else if($condition !== "Any" && $category === "All"){
+            // WHERE condition = $condition AND WHERE name LIKE $search
+            $this->db->where('condition', $condition);
+        }else{
+            // WHERE category = $category AND WHERE condition = $condition AND WHERE 'name' LIKE $search
+            $this->db->where('category', $category);
+            $this->db->where('condition', $condition);
         }
-        //gets results from above query
+
+        if($search !== ""){
+            $this->db->like('name', $search);
+        }
+
         $query = $this->db->get();
 
-        //returns query results in an array
-        return $query->result_array();*/
-
-       // SELECT * FROM Items
-       $this->db->select('*');
-       $this->db->from('Items');
-
-       if($category === "All" && $condition === "Any"){
-           // WHERE name LIKE $search
-           $this->db->like('name', $search);
-       }else if($category !== "All" && $condition === "All"){
-
-           // WHERE category = $category AND WHERE name LIKE $search
-           $this->db->where('category', $category);
-           $this->db->like('name',$search);
-       }else if($condition !== "Any" && $category === "All"){
-           // WHERE condition = $condition AND WHERE name LIKE $search
-           $this->db->where('condition', $condition);
-           $this->db->like('name',$search);
-       }else{
-           // WHERE category = $category AND WHERE condition = $condition AND WHERE 'name' LIKE $search
-           $this->db->where('category', $category);
-           $this->db->where('condition', $condition);
-           $this->db->like('name', $search);
-       }
-
-       $query = $this->db->get();
-
-       return $query->result_array();
+        return $query->result_array();
 
     }
 
