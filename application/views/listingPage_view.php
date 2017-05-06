bd5d1aa50c5d7fce1746b6976754ef96d9fe828f
Vivian Lee <vivianlee8295@gmail.com>
2017-05-05 22:55:44 -0700

    Remove sorting hat images from listing page view

--- application/views/listingPage_view.php
+++ application/views/listingPage_view.php
@@ -65,19 +65,6 @@
         <div class="col-lg-12">
             <h3 class=""></h3>
         </div>
-
-        <div class="col-sm-3 col-xs-6">
-            <a href="#">
-                <img class="img-responsive portfolio-item" src="<?php echo base_url()?>images/temp_images/s1.jpg" alt="">
-            </a>
-        </div>
-
-        <div class="col-sm-3 col-xs-6">
-            <a href="#">
-                <img class="img-responsive portfolio-item" src="<?php echo base_url()?>images/temp_images/s2.png" alt="">
-            </a>
-        </div>
-
     </div>
     <!-- /.row -->
 
