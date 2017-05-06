19c9517b0c51ffdf9106385e3392c9dea2e07a13
jonasvinter-jensen <jonasvinter-jensen@jonass-mac-mini>
2017-05-05 22:49:45 -0700

    Added error messages, XSS protection, keep input, @sfsu.edu and @mail.sfsu.edu and very general input format checks for the Registration page.

--- application/controllers/Registration.php
+++ application/controllers/Registration.php
@@ -1,53 +1,123 @@
 <?php
 
-class Registration extends CI_Controller
-{
+/**
+ * Created by PhpStorm.
+ * User: Nick_2
+ * Date: 4/10/2017
+ * Time: 7:30 PM
+ */
+class Registration extends CI_Controller {
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
+
+        /**
+         * and form helper
+         * Url helper is needed for bootstrap.
+         */
         $this->load->helper('form');
         $this->load->helper('url');
-        $this->load->library(array('session', 'form_validation', 'email'));
-        $this->load->database();
-        $this->load->model('Registration_model');
-        $this->load->model('Login_model');
+    }
+    
+    public function validate_input($data) {
+        return $this->security->xss_clean($data);
     }
 
-    public function index()
-    {
-        $this->register();
-    }
+    public function index() {
+        //Codeigniter preprocesses input for security
+        $this->load->helper(array('form'));
+        $this->load->library('form_validation');
+                
+        //Email rules
+        $this->form_validation->set_rules(
+                'email', 'Email', 'required|trim|valid_email', array(
+            'is_unique' => 'This %s already exists.'
+                )
+        );
+        /*WIP
+        if ($this->form_validation->run() == TRUE) {
+            //email_check('email')
+            $this->form_validation->set_rules('email', 'Email', 'callback_email_unique');
+        }*/
+        //Enforce hierachy: Default error messages first, custom error message second
+        if ($this->form_validation->run() == TRUE) {
+            //email_check('email')
+            $this->form_validation->set_rules('email', 'Email', 'callback_email_format');
+        }
 
-    public function register()
-    {
-        // Validate user input field constraints are not violated
-        if($this->input->post('register-submit'))
-            $this->form_validation->set_rules('username', 'username', 'required');
-            $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-            $this->form_validation->set_rules('password', 'password', 'required');
-            $this->form_validation->set_rules('password-confirm', 'confirm password', 'required|matches[password]');
 
-        if ($this->form_validation->run() == FALSE)
-        {
-            // Load the registration view
+        //Username rules
+        $this->form_validation->set_rules(
+                'username', 'Username', 'required|min_length[5]|max_length[12]'
+        );
+        /*$this->form_validation->set_rules(
+                'username', 'Username', array(
+            'required',
+            array($this->Registration_Model, 'email_unique')
+                )
+        );*/
+
+        $this->form_validation->set_rules('firstname', 'First Name', 'required|trim');
+        $this->form_validation->set_rules('lastname', 'Last Name', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[8]');
+        $this->form_validation->set_rules('confirmpassword', 'Confirm Password', 'required|trim|matches[password]');
+
+
+        //Second argument is the displayable name used for error messages
+        $email = $this->validate_input($this->input->post('email'));
+        $username = $this->validate_input($this->input->post('username'));
+        $firstname = $this->validate_input($this->input->post('firstname'));
+        $lastname = $this->validate_input($this->input->post('lastname'));
+        $password = $this->validate_input($this->input->post('password'));
+        $pass_confirm = $this->validate_input($this->input->post('confirmpassword'));
+
+        if ($this->form_validation->run() == FALSE) {
+            //If the email (and all other variables) had an incorrect format, do the following: 
             $title = array(
                 'title' => 'Registration');
             $this->load->view('header', $title);
             $this->load->view('registration_view');
             $this->load->view('footer');
         } else {
-            $data = array(
-                'username' => $this->input->post('username'),
-                'firstname' => $this->input->post('firstname'),
-                'lastname' => $this->input->post('lastname'),
-                'password' => $this->Login_model->encrypt($this->input->post('password')),
-                'email' => $this->input->post('email')
-            );
-
-            if ($this->Registration_model->insertNewUser($data)) {
-                redirect('/');
-            }
+            //Otherwise, load a 'registration succeeded' page
+            $title = array(
+                'title' => 'Registration success');
+            $this->load->view('header', $title);
+            $this->load->view('registration_success');
+            $this->load->view('footer');
         }
     }
+
+    //Pass 'email' as $str through the callback
+    public function email_format($str) {
+
+        //Checks if the given pattern is found in 'email' 
+        if (stristr($str, '@mail.sfsu.edu') !== false || stristr($str, '@sfsu.edu') !== false) {
+            return true;
+        }
+
+        $this->form_validation->set_message('email_format', 'Please provide a SFSU email address');
+        return false;
+    }
+    
+    public function email_unique($str) {
+        $this->load->database();
+        $query = $this->db->get('Users');
+        $query = $this->db->where('email', $str);
+ 
+        if ($query->num_rows() > 0) {
+            //echo $query;
+            return true;
+        } else {
+            return false;
+        }
+
+        /*if ($str == 'test@sfsu.edu') {
+            $this->form_validation->set_message('email_unique', 'The {field} field can not be the word "test"');
+            return FALSE;
+        } else {
+            return TRUE;
+        }*/
+    }
+
 }
--- application/views/registration_view.php
+++ application/views/registration_view.php
@@ -52,62 +52,74 @@
         ?>
         <div class="col-md-8 col-md-offset-2" id="formrow" >
 
-            <form class="form-horizontal" method="post">
-                <div class="form-group row">
-                    <label class="control-label col-md-4" for="username">Username: </label>
-                    <div class="col-md-8">
-                        <input type="text" class="form-control" id="username" name="username" placeholder="Username">
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label class="control-label col-md-4" for="username">First Name: </label>
-                    <div class="col-md-8">
-                        <input type="text" class="form-control" id="first-name" name="firstname" placeholder="First Name">
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label class="control-label col-md-4" for="username">Last Name: </label>
-                    <div class="col-md-8">
-                        <input type="text" class="form-control" id="last-name" name="lastname" placeholder="Last Name">
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label class="control-label col-md-4" for="password">Password: </label>
-                    <div class="col-md-8">
-                        <input type="text" class="form-control" id="password" name="password" placeholder="Enter Password">
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label class="control-label col-md-4" for="password">Confirm Password: </label>
-                    <div class="col-md-8">
-                        <input type="text" class="form-control" id="password-confirm" name="password-confirm" placeholder="Confirm Password">
-                    </div>
-                </div>
-
+            <form class="form-horizontal">
                 <!--$this->input->$post can only find the name of the text field-->
                 <div class="form-group row">
                     <label class="control-label col-md-4" for="email">SFSU Email: </label>
                     <div class="col-md-8">
                         <input style="text-align: left;" name="email" type="text" class="form-control" id="email" placeholder="Email"
                                value="<?php echo set_value('email'); ?>">
-                                <?php echo form_error('email', '<div class="error">', '</div>'); ?>
+                               <?php echo form_error('email', '<div class="error">', '</div>'); ?>     
+
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="control-label col-md-4" for="username">Username: </label>
+                    <div class="col-md-8">
+                        <input name="username" type="text" class="form-control" id="username" placeholder="Username"
+                               value="<?php echo set_value('username'); ?>">
+                               <?php echo form_error('username', '<div class="error">', '</div>'); ?> 
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="control-label col-md-4" for="username">First Name: </label>
+                    <div class="col-md-8">
+                        <input name="firstname" type="text" class="form-control" id="first-name" placeholder="First Name"
+                               value="<?php echo set_value('firstname'); ?>">
+                               <?php echo form_error('firstname', '<div class="error">', '</div>'); ?> 
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="control-label col-md-4" for="username">Last Name: </label>
+                    <div class="col-md-8">
+                        <input name="lastname" type="text" class="form-control" id="last-name" placeholder="Last Name"
+                               value="<?php echo set_value('lastname'); ?>">
+                               <?php echo form_error('lastname', '<div class="error">', '</div>'); ?> 
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="control-label col-md-4" for="password">Password: </label>
+                    <div class="col-md-8">
+                        <input name="password" type="text" class="form-control" id="password" placeholder="Enter Password"
+                               value="<?php echo set_value('password'); ?>">
+                               <?php echo form_error('password', '<div class="error">', '</div>'); ?> 
+
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="control-label col-md-4" for="password">Confirm Password: </label>
+                    <div class="col-md-8">
+                        <input name="confirmpassword" type="text" class="form-control" id="password-confirm" placeholder="Confirm Password"
+                               value="<?php echo set_value('confirmpassword'); ?>">
+                               <?php echo form_error('confirmpassword', '<div class="error">', '</div>'); ?> 
 
                     </div>
                 </div>
 
                 <div class="form-group row checkbox-inline" style="padding-left: 80px;">
-                    <label><input type="checkbox" style="height: 30px;" value="">I have read the terms & agreements.</label>
+                    <label><input type="checkbox" value="">I have read the terms & agreements.</label>
                 </div>
 
                 <div class="form-group row">
                     <a href="<?php echo base_url() ?>index.php/home" class="btn btn-danger" type="button" id="Cancel" >Cancel</a></button>
 
                     <!--<a href="<?php echo base_url() ?>index.php/registration/getInput" type="submit" id= "submit" class="btn btn-primary" style="float: right;">Create Account</a>-->
-                    <button style="float: right;" type="submit" id="submit" name="register-submit" class="btn btn-primary">Create Account</button>
+                    <button style="float: right;" type="submit" id="submit" class="btn btn-primary">Create Account</button>
                 </div>
                 <!--<input style="float: right;" type="submit" name="submit" value="Test"/>-->
             </form>
