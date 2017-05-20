d18ade82e916315eacb4ce7dfcfa8e4fcbf78f9e
jonasvinter-jensen <jonasvinter-jensen@jonass-mac-mini>
2017-05-19 18:16:51 -0700

    Merged this branch with latest development branch

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://sfsuse.com/~jonaskvj';
+$config['base_url'] = 'http://sfsuse.com/~vlee1';
 
 /*
 |--------------------------------------------------------------------------
@@ -449,10 +449,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-
-//When this is set to true, form_open will prevent CSRF attacks
-$config['csrf_protection'] = TRUE;
-
+$config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
