912d5dcd07b2e9e82415c091ae79cd41ab1264e5
Vivian Lee <vivianlee8295@gmail.com>
2017-05-21 16:12:09 -0700

    Fix password field on registration; comment out db escape causing conflicts with insertion to User

--- application/models/Registration_model.php
+++ application/models/Registration_model.php
@@ -21,10 +21,10 @@ class Registration_model extends CI_Model {
         //var_dump($data["firstname"]); Debugging
         //Password is not escaped, since it is not a string and should not be modified
         //escape() adds single quotes around string
-        $data["username"] = $this->db->escape($data["username"]); //Tested and works
+        //$data["username"] = $this->db->escape($data["username"]); //Tested and works
         $data["firstname"] = $this->db->escape($data["firstname"]);
         $data["lastname"] = $this->db->escape($data["lastname"]);
-        $data["email"] = $this->db->escape($data["email"]);
+        //$data["email"] = $this->db->escape($data["email"]);
         //var_dump($data["firstname"]); Debugging
         $this->db->insert("Users", $data);
         return true;
