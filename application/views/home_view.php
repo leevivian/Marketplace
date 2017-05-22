05b9fa8b82f3dfd6da4633d867bdd0bbd2f7f420
Vivian Lee <vivianlee8295@gmail.com>
2017-05-21 23:22:41 -0700

    Revert recent listings back to 6 instead of 12

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,41 +1,45 @@
 <html>
 <title>SFSUMarketplace</title>
 
-<h1 style="text-align: center; margin-top: 0px;">
-    <br>
-    <p style="color: black">Welcome to SFSUMarketplace!</p>
-    <p style="color: black">The online market to buy and sell goods with other SFSU connected people!</p>
-</h1>
+<head>
+    <h1 style="text-align: center; margin-top: 0px;">
+        <br>
+        <p style="color: black">Welcome to SFSUMarketplace!</p>
+        <p style="color: black">The online market to buy and sell goods with other SFSU connected people!</p>
+    </h1>
+</head>
 
-<style>
-    .carousel-inner > .item > img,
-    .carousel-inner > .item > a > img {
-        width: 100%;
-        margin: auto;
-        height: 260px;
-    }
-</style>
+<body>
 
-<div class="container-carousel">
+    <style>
+        .carousel-inner > .item > img,
+        .carousel-inner > .item > a > img {
+            width: 100%;
+            margin: auto;
+            height: 260px;
+        }
+    </style>
 
-    <!-- Title -->
-    <div class="row">
-        <div>
-            <h3 style="color:black">Most Recent Listings</h3>
+    <div class="container-carousel">
+
+        <!-- Title -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h3 style="color:black">Recent Listings</h3>
+            </div>
         </div>
-    </div>
-    <!-- /.row -->
+        <!-- /.row -->
 
-    <!-- Page Features -->
-    <div class="row text-center">
+        <!-- Page Features -->
+        <div class="row text-center">
 
-        <?php
-        if(isset($recent_listings)) {
-            foreach($recent_listings as $row) {
-                $id = $row['itemid'];
-                ?>
-                <div class='col-md-2 col-sm-6 hero-feature' style="min-height: 300px;">
-                    <div class='thumbnail'>
+            <?php
+            if(isset($recent_listings)) {
+                foreach($recent_listings as $row) {
+                    $id = $row['itemid'];
+                    ?>
+                    <div class='col-md-2 col-sm-6 hero-feature'>
+                        <div class='thumbnail''>
                         <img src="<?php echo base_url('images/item_images/' . 'thumbnail_' . $row['image']); ?>" alt="Image not found">
                         <div class="caption" style="176px;">
                             <h4><?php
@@ -55,68 +59,70 @@
                     </div>
                 </div>
                 <?php
+                }
             }
-        }
-        ?>
-
-    </div>
-</div>
-
-<div class="container-carousel">
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class=""></li>
-            <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-            <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
-        </ol>
-
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-            <div class="item active">
-                <a href="<?php echo base_url()?>index.php/search/category_books">
-                    <img src="<?php echo base_url()?>images/banner4.jpg" alt="banner1" width="460" height="300">
-                </a>
-                <div class="carousel-caption d-none d-md-block">
-                    <h3>Books</h3>
-                    <p>...Browse textbooks and other books other students are selling!</p>
-                </div>
-            </div>
-
-            <div class="item">
-                <a href="<?php echo base_url()?>index.php/search/category_furniture">
-                    <img src="<?php echo base_url()?>images/banner2.jpg" alt="banner2" width="460" height="300">
-                </a>
-                <div class="carousel-caption d-none d-md-block">
-                    <h3>Furniture</h3>
-                    <p>Get your hands on some rad furniture at low prices!</p>
-                </div>
-            </div>
-
-            <div class="item">
-                <a href="<?php echo base_url()?>index.php/search/category_electronics">
-                    <img src="<?php echo base_url()?>images/banner3.jpg" alt="banner3" width="460" height="300">
-                </a>
-                <div class="carousel-caption d-none d-md-block">
-                    <h3>Electronics</h3>
-                    <p>Get the latest technology in the city!</p>
-                </div>
-            </div>
+            ?>
 
         </div>
-
-        <!-- Left and right controls -->
-        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
     </div>
-</div>
 
+    <div class="container-carousel">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <a href="<?php echo base_url()?>index.php/search/category_books">
+                        <img src="<?php echo base_url()?>images/banner4.jpg" alt="banner1" width="460" height="300">
+                    </a>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Books</h3>
+                        <p>...Browse textbooks and other books other students are selling!</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <a href="<?php echo base_url()?>index.php/search/category_furniture">
+                        <img src="<?php echo base_url()?>images/banner2.jpg" alt="banner2" width="460" height="300">
+                    </a>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Furniture</h3>
+                        <p>Get your hands on some rad furniture at low prices!</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <a href="<?php echo base_url()?>index.php/search/category_electronics">
+                        <img src="<?php echo base_url()?>images/banner3.jpg" alt="banner3" width="460" height="300">
+                    </a>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h3>Electronics</h3>
+                        <p>Get the latest technology in the city!</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+
+</body>
 <style>
 
     .container-carousel {
@@ -128,4 +134,6 @@
 
 </style>
 
-</body></html>
+
+</html>
+
