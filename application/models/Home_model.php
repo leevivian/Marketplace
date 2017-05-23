59ebb1d69de839ac21431874175cdfb97ec3917d
AJ Culanay <aculanay@mail.sfsu.edu>
2017-05-23 13:59:57 -0700

    Revert "Merge branch 'aj' into development"
    This reverts commit 600ec3a87ecf772e6a88ffb4f3120d27590cb6dc, reversing
    changes made to 2ae3dcf23c980e571a7e25755a9ec7d98205bbd6.

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -10,7 +10,7 @@ class Home_model extends CI_Model
     // Sends back an array containing six most recent listings to be displayed
     // on the Home Page.
     public function getRecentListings() {
-        $query = $this->db->query("SELECT * FROM Items ORDER BY itemid DESC LIMIT 6");
+        $query = $this->db->query("SELECT * FROM Items ORDER BY itemid DESC LIMIT 12");
         return $query->result_array();
     }
 
